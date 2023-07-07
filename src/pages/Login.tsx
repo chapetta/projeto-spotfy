4f3dab217f0ee1aca6f8156af0dc8d9feab99d72
chapetta <cha.petta@hotmail.com>
2023-07-06 21:21:55 -0300

    Fazendo a pagina de login, adiconando input, botao, e validaçao

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,12 +1,38 @@
+/* eslint-disable react/jsx-curly-spacing */
+import { useState, useEffect } from 'react';
+
 function Login() {
+  const [validateButton, setValidateButton] = useState(true);
+  const [inputValue, setInputValue] = useState('');
+
+  function validateLogin() {
+    if (inputValue.length >= 3) {
+      setValidateButton(false);
+    } else {
+      setValidateButton(true);
+    }
+  }
+  useEffect(() => {
+    validateLogin();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [inputValue]);
   return (
     <div>
       <h1>ChapsTunes</h1>
       <form>
         <label>
-          <input placeholder="Nome" />
+          <input
+            placeholder="Nome"
+            value={inputValue}
+            onChange={({ target }) => setInputValue(target.value)}
+          />
         </label>
-        <button>Entrar</button>
+        <button
+          disabled={validateButton}
+        >
+          Entrar
+
+        </button>
       </form>
     </div>
   );
