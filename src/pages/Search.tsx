c167bda16884443a2d79fcbb5db05d2653af213c
chapetta <cha.petta@hotmail.com>
2023-07-12 19:54:10 -0300

    implementando o botao de pesquisar artistas

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,10 +1,42 @@
+/* eslint-disable react/jsx-curly-spacing */
+import { useState, useEffect } from 'react';
 import Header from '../componentes/Header';
 
 function Search() {
+  const [validateButton, setValidateButton] = useState(true);
+  const [inputValue, setInputValue] = useState('');
+
+  function handleButton() {
+    if (inputValue.length >= 2) {
+      setValidateButton(false);
+    } else {
+      setValidateButton(true);
+    }
+  }
+
+  useEffect(() => {
+    handleButton();
+  }, [inputValue]);
+
   return (
     <div>
       <Header />
-      Search
+      <form>
+        <label>
+          <input
+            value={inputValue}
+            onChange={({ target }) => setInputValue(target.value)}
+            placeholder="Nome do Artista"
+          />
+        </label>
+        <button
+          // eslint-disable-next-line react/jsx-curly-spacing
+          disabled={validateButton}
+        >
+          Pesquisar
+
+        </button>
+      </form>
     </div>
   );
 }
